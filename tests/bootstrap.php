166810aaae829cc6b321ad76c248d54dea27af0b
tarlepp <tarmo.leppanen@protacon.com>
2021-04-14 16:50:13 +0300

    Updated dependencies, added support for PHP 8 and protacon.com => pinja.com

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 /**
  * /tests/bootstrap.php
  *
- * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
  */
 
 // TODO: add necessary bootstrap for tests.
